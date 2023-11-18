ea5e306a7826a6793bdb9f53f70504dcc4b00e15
Janderson <jandersonsouzagit@gmail.com>
2023-11-18 16:54:55 -0300

    Classe HistoricoFisioterapeutico

---
+++ models/HistoricoFisioterapeutico.php
@@ -0,0 +1,56 @@
+<?php
+
+class HisotoricoFisioterapeutico{
+    private int $id;
+    private string $tratamentoAnterior;
+    private string $resultadoTratamentoAnterior;
+    private string $problemaFisicoRecorrente;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): self
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    public function getTratamentoAnterior(): string
+    {
+        return $this->tratamentoAnterior;
+    }
+
+    public function setTratamentoAnterior(string $tratamentoAnterior): self
+    {
+        $this->tratamentoAnterior = $tratamentoAnterior;
+
+        return $this;
+    }
+
+    public function getResultadoTratamentoAnterior(): string
+    {
+        return $this->resultadoTratamentoAnterior;
+    }
+
+    public function setResultadoTratamentoAnterior(string $resultadoTratamentoAnterior): self
+    {
+        $this->resultadoTratamentoAnterior = $resultadoTratamentoAnterior;
+
+        return $this;
+    }
+
+    public function getProblemaFisicoRecorrente(): string
+    {
+        return $this->problemaFisicoRecorrente;
+    }
+
+    public function setProblemaFisicoRecorrente(string $problemaFisicoRecorrente): self
+    {
+        $this->problemaFisicoRecorrente = $problemaFisicoRecorrente;
+
+        return $this;
+    }
+}
