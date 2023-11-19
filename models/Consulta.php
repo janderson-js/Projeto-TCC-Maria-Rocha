09eeee648b746068119a5e4c94654a628f5ff3c0
Janderson <jandersonsouzagit@gmail.com>
2023-11-18 22:04:32 -0300

    Classe Consulta

---
+++ models/Consulta.php
@@ -0,0 +1,111 @@
+<?php
+
+class Consulta{
+    private int $id;
+    private DateTime $data;
+    private string $hora;
+    private string $tipo;
+    private string $observacoesEspecificas;
+    private string $procedimentosOuTratamentosRealizados;
+
+    private Paciente $paciente;
+    private Funcionario $funcionario;
+
+    
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): self
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    public function getData(): DateTime
+    {
+        return $this->data;
+    }
+
+    public function setData(DateTime $data): self
+    {
+        $this->data = $data;
+
+        return $this;
+    }
+
+    public function getHora(): string
+    {
+        return $this->hora;
+    }
+
+    public function setHora(string $hora): self
+    {
+        $this->hora = $hora;
+
+        return $this;
+    }
+
+    public function getTipo(): string
+    {
+        return $this->tipo;
+    }
+
+    public function setTipo(string $tipo): self
+    {
+        $this->tipo = $tipo;
+
+        return $this;
+    }
+
+    public function getObservacoesEspecificas(): string
+    {
+        return $this->observacoesEspecificas;
+    }
+
+    public function setObservacoesEspecificas(string $observacoesEspecificas): self
+    {
+        $this->observacoesEspecificas = $observacoesEspecificas;
+
+        return $this;
+    }
+
+    public function getProcedimentosOuTratamentosRealizados(): string
+    {
+        return $this->procedimentosOuTratamentosRealizados;
+    }
+
+    public function setProcedimentosOuTratamentosRealizados(string $procedimentosOuTratamentosRealizados): self
+    {
+        $this->procedimentosOuTratamentosRealizados = $procedimentosOuTratamentosRealizados;
+
+        return $this;
+    }
+
+    public function getPaciente(): Paciente
+    {
+        return $this->paciente;
+    }
+
+    public function setPaciente(Paciente $paciente): self
+    {
+        $this->paciente = $paciente;
+
+        return $this;
+    }
+
+    public function getFuncionario(): Funcionario
+    {
+        return $this->funcionario;
+    }
+
+    public function setFuncionario(Funcionario $funcionario): self
+    {
+        $this->funcionario = $funcionario;
+
+        return $this;
+    }
+}
