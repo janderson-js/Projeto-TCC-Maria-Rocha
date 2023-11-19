eee193b361f71f0e7ee044b49b6cc8192e42e811
Janderson <jandersonsouzagit@gmail.com>
2023-11-18 21:22:16 -0300

    Classe Servico

---
+++ models/Servico.php
@@ -0,0 +1,56 @@
+<?php
+
+class Servico{
+    private int $id;
+    private string $nome;
+    private string $descricao;
+    private string $tipo;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): self
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    public function getNome(): string
+    {
+        return $this->nome;
+    }
+
+    public function setNome(string $nome): self
+    {
+        $this->nome = $nome;
+
+        return $this;
+    }
+
+    public function getDescricao(): string
+    {
+        return $this->descricao;
+    }
+
+    public function setDescricao(string $descricao): self
+    {
+        $this->descricao = $descricao;
+
+        return $this;
+    }
+
+    public function getTipo(): string
+    {
+        return $this->tipo;
+    }
+
+    public function setTipo(string $tipo): self
+    {
+        $this->tipo = $tipo;
+
+        return $this;
+    }
+}
