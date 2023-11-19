3124b447a080f61546411572ad9986d1941a1cc8
Janderson <jandersonsouzagit@gmail.com>
2023-11-18 21:27:48 -0300

    Classe Especialidade

---
+++ models/Especialidade.php
@@ -0,0 +1,43 @@
+<?php
+
+class Especialidade{
+    private int $id;
+    private string $titulo;
+    private string $descricao;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): self
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    public function getTitulo(): string
+    {
+        return $this->titulo;
+    }
+
+    public function setTitulo(string $titulo): self
+    {
+        $this->titulo = $titulo;
+
+        return $this;
+    }
+
+    public function getDescricao(): string
+    {
+        return $this->descricao;
+    }
+
+    public function setDescricao(string $descricao): self
+    {
+        $this->descricao = $descricao;
+
+        return $this;
+    }
+}
