59c2963f39bdc87f1689d754200aeb7b3f140349
Janderson <jandersonsouzagit@gmail.com>
2023-11-18 21:35:18 -0300

    Classe Menu

---
+++ models/Menu.php
@@ -0,0 +1,56 @@
+<?php
+
+class Menu{
+    private int $id;
+    private string $titulo;
+    private string $descricao;
+    private string $url;  
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): self
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    public function getTitulo(): string
+    {
+        return $this->titulo;
+    }
+
+    public function setTitulo(string $titulo): self
+    {
+        $this->titulo = $titulo;
+
+        return $this;
+    }
+
+    public function getDescricao(): string
+    {
+        return $this->descricao;
+    }
+
+    public function setDescricao(string $descricao): self
+    {
+        $this->descricao = $descricao;
+
+        return $this;
+    }
+
+    public function getUrl(): string
+    {
+        return $this->url;
+    }
+
+    public function setUrl(string $url): self
+    {
+        $this->url = $url;
+
+        return $this;
+    }
+}
