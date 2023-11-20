c35a509836928f708407082ff36461f9137357d9
Janderson Souza <jandersonsouzagit@gmail.com>
2023-11-20 15:23:14 -0300

    Classe PerfilDAO - metodo carregarPorId

--- dao/PerfilDAO.php
+++ dao/PerfilDAO.php
@@ -53,6 +53,32 @@ class PerfilDAO{
         }
     }
 
-    
+    public function carregaPorIdPerfil(int $id){
+
+        $sqlCarregaPorIdPerfil = "SELECT * FROM Perfil WHERE id=':id'";
+
+        try {
+            $stmt = $this->conn->getConexao()->prepare($sqlCarregaPorIdPerfil);
+            $stmt->bindValue(":id", $id, PDO::PARAM_INT);
+
+            $stmt->execute();
+
+            $resul = $stmt->fetchALL(PDO::FETCH_ASSOC);
+            
+            foreach($resul as $row){
+                $perfil[] = [
+                    'id' => $row['id'],
+                    'titulo' => $row['titulo'],
+                    'descricao' => $row['descricao']
+                ];
+            }
+        return $perfil;
+
+        } catch (\PDOException $e) {
+            error_log("Erro ao carregar por id o Perfil: " . $e->getMessage());
+        }finally{
+            $this->conn->desconectar();
+        }
+    }
     
 }
