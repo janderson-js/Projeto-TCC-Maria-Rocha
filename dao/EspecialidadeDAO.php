bd33e6711f2021e57bc0328eae1abc7e9debb031
Janderson Souza <jandersonsouzagit@gmail.com>
2023-11-20 15:33:42 -0300

    Classe EspecialidadeDAO - metodo editarEspecialidade

--- dao/EspecialidadeDAO.php
+++ dao/EspecialidadeDAO.php
@@ -29,4 +29,28 @@ class EspecialidadeDAO{
             $this->conn->desconectar();
         }
     }
+
+    public function editarEspecialidade(Especialidade $especialidade){
+
+        $sqlEditarEspecialidade = "UPDATE especialidade SET 
+        titulo=':titulo', 
+        descricao=':descricao',
+        WHERE id=':id'";
+
+        try {
+            $stmt = $this->conn->getConexao()->prepare($sqlEditarEspecialidade);
+            $stmt->bindValue(":titulo", $especialidade->getTitulo(), PDO::PARAM_STR);
+            $stmt->bindValue(":descricao", $especialidade->getDescricao(), PDO::PARAM_STR);
+            $stmt->bindValue(":id", $especialidade->getId(), PDO::PARAM_INT);
+
+            $stmt->execute();
+            
+
+        } catch (\PDOException $e) {
+            error_log("Erro ao editar Especialidade:" . $e->getMessage());
+        }finally{
+            $this->conn->desconectar();
+        }
+    }
+
 }
