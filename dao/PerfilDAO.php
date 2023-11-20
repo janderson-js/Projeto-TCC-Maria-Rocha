f63d0d36239d1d4da15aa7361f8ec9dd6d7ad5c1
Janderson Souza <jandersonsouzagit@gmail.com>
2023-11-20 15:19:49 -0300

    Classe PerfilDAO - metodo editarPerfil

--- dao/PerfilDAO.php
+++ dao/PerfilDAO.php
@@ -12,7 +12,7 @@ class PerfilDAO{
 
     public function inserirPerfil(Perfil $perfil){
 
-        $sqlInserirPerfil = "INSERT INTO Perfil (titulo, descricao, url) VALUES
+        $sqlInserirPerfil = "INSERT INTO perfil (titulo, descricao, url) VALUES
         (:titulo, :descricao, :url)";
 
         try {
@@ -24,11 +24,35 @@ class PerfilDAO{
             
 
         } catch (\PDOException $e) {
-            error_log("Erro ao inserir Perfil:" . $e->getMessage());
+            error_log("Erro ao inserir perfil:" . $e->getMessage());
         }finally{
             $this->conn->desconectar();
         }
     }
 
+    public function editarPerfil(Perfil $perfil){
+
+        $sqlEditarPerfil = "UPDATE perfil SET 
+        titulo=':titulo', 
+        descricao=':descricao',
+        WHERE id=':id'";
+
+        try {
+            $stmt = $this->conn->getConexao()->prepare($sqlEditarPerfil);
+            $stmt->bindValue(":titulo", $perfil->getTitulo(), PDO::PARAM_STR);
+            $stmt->bindValue(":descricao", $perfil->getDescricao(), PDO::PARAM_STR);
+            $stmt->bindValue(":id", $perfil->getId(), PDO::PARAM_INT);
+
+            $stmt->execute();
+            
+
+        } catch (\PDOException $e) {
+            error_log("Erro ao editar Perfil:" . $e->getMessage());
+        }finally{
+            $this->conn->desconectar();
+        }
+    }
+
+    
     
 }
