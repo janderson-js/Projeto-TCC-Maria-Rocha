18d4c246ff0bcb3647361e3a30207e0425adc4c0
Janderson <jandersonsouzagit@gmail.com>
2023-11-21 20:35:18 -0300

    Classe HistoricoFisioterapeuticoDAO - Metodo editarHistoricoFisioterapeutico

--- dao/HistoricoFisioterapeuticoDAO.php
+++ dao/HistoricoFisioterapeuticoDAO.php
@@ -23,17 +23,39 @@ class HistoricoFisioterapeuticoDAO{
             $stmt->bindValue(":motivo_tratamento_anterior", $historicoFisioterapeutico->getMotivoTratamentoAnterior(), PDO::PARAM_STR);
             $stmt->bindValue(":resultado_tratamento_anterior", $historicoFisioterapeutico->getResultadoTratamentoAnterior(), PDO::PARAM_STR);
             $stmt->bindValue(":problema_fisico_recorrente", $historicoFisioterapeutico->getProblemaFisicoRecorrente(), PDO::PARAM_STR);
-            
 
             $stmt->execute();
             
-
         } catch (\PDOException $e) {
-            error_log("Erro ao inserir HistoricoFisioterapeutico:" . $e->getMessage());
+            error_log("Erro ao inserir Historico Fisioterapeutico:" . $e->getMessage());
         }finally{
             $this->conn->desconectar();
         }
     }
     
-  
+    public function editarHistoricoMedico(HisotoricoFisioterapeutico $historicoFisioterapeutico){
+
+        $sqlEditarHistoricoFisioterapeutico = "UPDATE historico_fisioterapeutico SET 
+        tratamento_anterior=':tratamento_anterior',
+        motivo_tratamento_anterior=':motivo_tratamento_anterior',
+        resultado_tratamento_anterior='resultado_tratamento_anterior',
+        problema_fisico_recorrente='problema_fisico_recorrente'
+        WHERE id=':id'";
+
+        try {
+            $stmt = $this->conn->getConexao()->prepare($sqlEditarHistoricoFisioterapeutico);
+            $stmt->bindValue(":tratamento_anterior", $historicoFisioterapeutico->getTratamentoAnterior(), PDO::PARAM_STR);
+            $stmt->bindValue(":motivo_tratamento_anterior", $historicoFisioterapeutico->getMotivoTratamentoAnterior(), PDO::PARAM_STR);
+            $stmt->bindValue(":resultado_tratamento_anterior", $historicoFisioterapeutico->getResultadoTratamentoAnterior(), PDO::PARAM_STR);
+            $stmt->bindValue(":problema_fisico_recorrente", $historicoFisioterapeutico->getProblemaFisicoRecorrente(), PDO::PARAM_STR);
+            $stmt->bindValue(":id", $historicoFisioterapeutico->getId(), PDO::PARAM_INT);
+
+            $stmt->execute();           
+
+        } catch (\PDOException $e) {
+            error_log("Erro ao editar Historico Medico:" . $e->getMessage());
+        }finally{
+            $this->conn->desconectar();
+        }
+    }
 }
